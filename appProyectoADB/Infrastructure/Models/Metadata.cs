51ec2e6b4ca37085f93c5cabee9f0c7f32d41f6c
brojasl90 <72952118+brojasl90@users.noreply.github.com>
2021-10-25 01:29:21 -0600

    Agregando archivo metadata Producto
    Agregando archivo metadata: Infra > Models > Metadata

---
+++ appProyectoADB/Infrastructure/Models/Metadata.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Models
+{
+    internal partial class ProductoMetadata
+    {
+        public int IdProducto { get; set; }
+
+        [Display(Name = "Categoria")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public int IdCategoria { get; set; }
+
+        [Display(Name = "Nombre Producto")]
+        public string Nombre_Producto { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public Nullable<decimal> Precio { get; set; }
+
+        [Display(Name = "Fecha de creación")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public Nullable<System.DateTime> FechaCreacion { get; set; }
+
+        [Display(Name = "Fecha de vencimiento")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public Nullable<System.DateTime> FechaVence { get; set; }
+
+        public Nullable<byte> Estado { get; set; }
+
+        [Display(Name = "Cantidad Disponible")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public Nullable<int> CantidadDisponible { get; set; }
+
+        [Display(Name = "Cantidad minima requerida")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public Nullable<int> CantidadStockMin { get; set; }
+
+        [Display(Name = "Cantidad máxima requerida")]
+        [Required(ErrorMessage = "{0} es un dato requerido")]
+        public Nullable<int> CantidadStockMax { get; set; }
+    }
+}
