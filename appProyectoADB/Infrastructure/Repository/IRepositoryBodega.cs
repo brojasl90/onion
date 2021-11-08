af1d62a16e23bf46210a2827353e9e7dec7b9376
brojasl90 <72952118+brojasl90@users.noreply.github.com>
2021-11-07 20:49:37 -0600

    Creacion RepoBodega
    Creacion RepoBodega

---
+++ appProyectoADB/Infrastructure/Repository/IRepositoryBodega.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public interface IRepositoryBodega
+    {
+        IEnumerable<Bodega> GetBodega();
+        Bodega GetBodegaByID(int id);
+
+        Bodega Save(Bodega pBodega);
+        void DeleteBodega(int id);
+    }
+}
