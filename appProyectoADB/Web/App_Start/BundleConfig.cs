e2978f6935ebbe39c96e018a79a39ca93109545a
MGueL <mi.migue437@gmail.com>
2021-11-12 19:57:34 -0600

    Agregando Ajax.Unobtrusive

--- appProyectoADB/Web/App_Start/BundleConfig.cs
+++ appProyectoADB/Web/App_Start/BundleConfig.cs
@@ -1,4 +1,4 @@
-﻿using System.Web;
+﻿ using System.Web;
 using System.Web.Optimization;
 
 namespace Web
@@ -13,6 +13,9 @@ namespace Web
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryajax").Include(
+                        "~/Scripts/jquery.unobtrusive*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
