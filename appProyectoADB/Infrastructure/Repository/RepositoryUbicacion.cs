7bf4d29e33d94f960bed44837cf6ea96dd39f487
brojasl90 <72952118+brojasl90@users.noreply.github.com>
2021-11-18 20:55:42 -0600

    Creacion de ubicacion
    Creacion de ubicacion

---
+++ appProyectoADB/Infrastructure/Repository/RepositoryUbicacion.cs
@@ -0,0 +1,70 @@
+﻿using Infrastructure.Models;
+using Infrastructure.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repository
+{
+    public class RepositoryUbicacion : IRepositoryUbicacion
+    {
+        public IEnumerable<Ubicacion> GetUbicacion()
+        {
+            try
+            {
+
+                IEnumerable<Ubicacion> lista = null;
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    //Select * from Ubicacion
+                    //lista = ctx.Ubicacion.ToList<Ubicacion>();
+                    lista = ctx.Ubicacion.Include("Bodega").ToList<Ubicacion>();
+                }
+                return lista;
+            }
+
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+
+        public Ubicacion GetUbicacionByID(int id)
+        {
+            Ubicacion oUbicacion = null;
+            try
+            {
+                using (MyContext ctx = new MyContext())
+                {
+                    ctx.Configuration.LazyLoadingEnabled = false;
+                    oUbicacion = ctx.Ubicacion.Find(id);
+                }
+                return oUbicacion;
+            }
+            catch (DbUpdateException dbEx)
+            {
+                string mensaje = "";
+                Log.Error(dbEx, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw new Exception(mensaje);
+            }
+            catch (Exception ex)
+            {
+                string mensaje = "";
+                Log.Error(ex, System.Reflection.MethodBase.GetCurrentMethod(), ref mensaje);
+                throw;
+            }
+        }
+    }
+}
