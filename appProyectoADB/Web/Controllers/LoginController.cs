6f9d0cc42aaebefab917f842038f2204b33141e3
MGueL <mi.migue437@gmail.com>
2021-11-18 21:59:12 -0600

    Insertar LoginController

---
+++ appProyectoADB/Web/Controllers/LoginController.cs
@@ -0,0 +1,103 @@
+﻿using ApplicationCore.Services;
+using Infrastructure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Web.Enum;
+using Web.Utils;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login(Usuario pUsuario)
+        {
+            IServiceUsuario _ServiceUsuario = new ServiceUsuario();
+            Usuario oUsuario = null;
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    oUsuario = _ServiceUsuario.GetUsuario(pUsuario.Correo, pUsuario.Clave);
+
+                    if (oUsuario != null)
+                    {
+                        Session["User"] = oUsuario;
+                        Log.Info($"Accede {oUsuario.Nombre} {oUsuario.PrimerApellido} {oUsuario.SegundoApellido} con el rol {oUsuario.Rol.IdRol}-{oUsuario.Rol.Descripcion}");
+                        TempData["mensaje"] = SweetAlertHelper.Mensaje("Login", "Usario autenticado satisfactoriamente", SweetAlertMessageType.success);
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        Log.Warn($"{pUsuario.Correo} se intentó conectar  y falló");
+                        ViewBag.NotificationMessage = SweetAlertHelper.Mensaje("Login", "Error al autenticarse", SweetAlertMessageType.warning);
+
+                    }
+                }
+
+                return View("Index");
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                // Pasar el Error a la página que lo muestra
+                TempData["Message"] = ex.Message;
+                TempData.Keep();
+                return RedirectToAction("Default", "Error");
+            }
+        }
+        public ActionResult UnAuthorized()
+        {
+            try
+            {
+                ViewBag.Message = "No autorizado";
+
+                if (Session["User"] != null)
+                {
+                    Usuario oUsuario = Session["User"] as Usuario;
+                    Log.Warn($"El usuario {oUsuario.Nombre} {oUsuario.PrimerApellido} {oUsuario.SegundoApellido}  con el rol {oUsuario.Rol.IdRol}-{oUsuario.Rol.Descripcion}, intentó acceder una página sin permisos  ");
+                }
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                // Pasar el Error a la página que lo muestra
+                TempData["Message"] = ex.Message;
+                TempData["Redirect"] = "Login";
+                TempData["Redirect-Action"] = "Index";
+                return RedirectToAction("Default", "Error");
+            }
+        }
+        public ActionResult Logout()
+        {
+            try
+            {
+                Log.Info("Usuario desconectado!");
+                Session["User"] = null;
+                return RedirectToAction("Index", "Login");
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                // Pasar el Error a la página que lo muestra
+                TempData["Message"] = ex.Message;
+                TempData["Redirect"] = "Login";
+                TempData["Redirect-Action"] = "Index";
+                return RedirectToAction("Default", "Error");
+            }
+        }
+    }
+}
