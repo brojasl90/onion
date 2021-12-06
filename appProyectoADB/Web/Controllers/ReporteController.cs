0ea78bc129c7b15e17954ee4ca42d6fa7c76759a
brojasl90 <72952118+brojasl90@users.noreply.github.com>
2021-12-05 22:58:06 -0600

    Reporte Producto
    Creando ReporteController, graficoChartUS, VIewModelGrafico, graficoProducto

---
+++ appProyectoADB/Web/Controllers/ReporteController.cs
@@ -0,0 +1,41 @@
+﻿using ApplicationCore.Services;
+using Infrastructure.Models;
+using Infrastructure.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    public class ReporteController : Controller
+    {
+        // GET: Reporte
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductoCatalogo()
+        {
+            IEnumerable<Producto> lista = null;
+            try
+            {
+
+                IServiceProducto _ServiceProducto = new ServiceProducto();
+                lista = _ServiceProducto.GetProducto();
+                return View(lista);
+            }
+            catch (Exception ex)
+            {
+                // Salvar el error en un archivo 
+                Log.Error(ex, MethodBase.GetCurrentMethod());
+                TempData["Message"] = "Error al procesar los datos! " + ex.Message;
+                TempData.Keep();
+                // Redireccion a la captura del Error
+                return RedirectToAction("Default", "Error");
+            }
+        }
+    }
+}
---
+++ appProyectoADB/Web/Scripts/graficoChartJS.js
@@ -0,0 +1,41 @@
+﻿function grafico(titulo, tituloEtiquetas, tipo, colores, etiquetas, valores) {
+    var data =
+    {
+        labels: etiquetas,
+        datasets: [{
+            label: titulo,
+            backgroundColor: colores,
+            borderWidth: 2,
+            data: valores
+        }]
+    };
+
+    var ctx1 = document.getElementById("grafico").getContext("2d");
+    window.myBar = new Chart(ctx1,
+        {
+            type: tipo,
+            data: data,
+            options:
+            {
+
+                animation: {
+                    duration: 1000 // general animation time
+                },
+
+                hover: {
+                    animationDuration: 3000 // duration of animations when hovering an item
+                },
+                responsiveAnimationDuration: 3000, // animation duration after a resize
+                legend: { display: true },
+                title:
+                {
+                    display: true,
+                    text: tituloEtiquetas
+                },
+                responsive: true,
+                maintainAspectRatio: true,
+
+            }
+        });
+
+}
